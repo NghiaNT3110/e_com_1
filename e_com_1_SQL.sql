3d5eb1b5bf5edbab7aa6d151e9312923f1086074
Nguyen Trung Nghia <156154939+NghiaNT3110@users.noreply.github.com>
2024-01-15 17:22:01 +0700

    Thêm các comment giải thích vào từng dòng để tăng độ dễ đọc của code

--- e_com_1_SQL.sql
+++ e_com_1_SQL.sql
@@ -1,22 +1,22 @@
 /* Lựa chọn các trường định vị đơn hàng (ID, trạng thái), nhân khẩu học, nguồn traffic từ các bảng thông tin */
 
-SELECT DISTINCT inventory_item_id, 
-  orders.order_id,
-  users.first_name ||" "|| users.last_name as full_name, 
-  users.email, users.age, users.city,users.state,users.country,order_items.status,traffic_source,
-  sale_price, inventory_items.cost, 
-  orders.num_of_item,
+SELECT DISTINCT inventory_item_id, /* Chọn Item ID trong kho không trùng vì mỗi Order ID gắn liền với Item ID trong kho */ 
+  orders.order_id, /* Chọn Order ID */ 
+  users.first_name ||" "|| users.last_name as full_name, /* Gộp họ và tên thành đầy đủ họ tên */ 
+  users.email, users.age, users.city,users.state,users.country,order_items.status,traffic_source, /* Chọn các trường thông tin về người dùng và traffic */ 
+  sale_price, inventory_items.cost, /* Chọn giá bán và chi phí nhập hàng */
+  orders.num_of_item, /* Số lượng item được người dùng đặt mua trong đơn hàng */ 
 
 /* Tính toán chi phí, doanh thu và lơi nhuận */
 
-  inventory_items.cost * orders.num_of_item as total_product_cost,
-  orders.num_of_item * sale_price as total_revenue_purchase,
-  orders.num_of_item * (sale_price - inventory_items.cost) as profit,
+  inventory_items.cost * orders.num_of_item as total_product_cost, /* Chi phí */
+  orders.num_of_item * sale_price as total_revenue_purchase, /* Doanh thu */ 
+  orders.num_of_item * (sale_price - inventory_items.cost) as profit, /* Lợi nhuận */ 
 
  /* Chọn các trường thông tin về phân loại sản phẩm và tên sản phẩm */
 
-  inventory_items.product_category,inventory_items.product_name, 
-  inventory_items.product_department, inventory_items.product_brand,
+  inventory_items.product_category,inventory_items.product_name, /* Chọn trường phân loại sản phẩm và tên sản phẩm */
+  inventory_items.product_department,inventory_items.product_brand, /* Chọn giới tính theo sản phẩm và thương hiệu sản phẩm */ 
 
 /* Chọn các trường thông tin liên quan đến vòng đời ngày của đơn hàng */ 
 
@@ -25,18 +25,19 @@ SELECT DISTINCT inventory_item_id,
 /* Tính toán số tiền bị hoàn đơn */ 
 
 CASE 
-WHEN order_items.status LIKE '%Return%' OR order_items.status LIKE '%Cancel%'
+WHEN order_items.status LIKE '%Return%' OR order_items.status LIKE '%Cancel%' /* Khi trạng thái của đơn giống hoàn đơn hoặc hủy đơn */
 THEN orders.num_of_item * sale_price
-else 0
+else 0 /* Nếu không phải 2 trạng thái đó thì mặc định là 0 */ 
 end as total_return,
+
 
 /* Tính toán khoảng cách ngày để xử lý đơn hàng, giao đơn hàng, trả đơn hàng sau khi nhận được hàng */ 
 
-DATE_DIFF(order_items.delivered_at , order_items.shipped_at, DAY) as delivery_time_in_days,
-DATE_DIFF(order_items.shipped_at , order_items.created_at, DAY) as process_time_in_days,
-DATE_DIFF(order_items.returned_at , order_items.created_at, DAY) as return_after_received_days
+DATE_DIFF(order_items.delivered_at , order_items.shipped_at, DAY) as delivery_time_in_days, /* Thời gian giao hàng từ khi ra khỏi kho */ 
+DATE_DIFF(order_items.shipped_at , order_items.created_at, DAY) as process_time_in_days, /* Thời gian tạo đơn đến lúc đơn được ship ra khỏi kho */ 
+DATE_DIFF(order_items.returned_at , order_items.created_at, DAY) as return_after_received_days /* Thời gian tạo đơn đến lúc người nhận đã có hàng và gửi lại do không hài lòng/lỗi */ 
 
-FROM big-query-378507.thelook_ecommerce.order_items
+FROM big-query-378507.thelook_ecommerce.order_items /* Bảng chính được dùng */ 
 
 /* Join bảng Users để lấy thông tin về các trường nhân khẩu học, traffic */
   LEFT JOIN big-query-378507.thelook_ecommerce.users
